8997dd227c9ec6b814a9b8d91e57b322c3df59d8
Davi Vitorino da Silva <davi.silva@softplan.com.br>
2024-07-07 12:46:06 -0300

    feat: add selection and insertion sort

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from bubble_sort import bubble_sort
+from selection_sort import selection_sort
+from insertion_sort import insertion_sort
 
 arr = [4, 1, 5, 9, 2, 6, 2, 8, 5, 6, 0, -1]
 
 print(bubble_sort(arr))
+print(selection_sort(arr))
+print(insertion_sort(arr))
